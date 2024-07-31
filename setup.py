3548b208c5a5b77d2593609c3a1bb31ec4dc0b37
Lucas Lago <lukemartinslagonit@gmail.com>
2024-07-31 00:52:12 -0300

    fix import judge path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plotly-agent',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT License',
     packages=find_packages(),
     install_requires=[
