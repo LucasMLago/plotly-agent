2048dee377281a6021d3f8a25534066bd45e9329
Lucas Lago <lukemartinslagonit@gmail.com>
2024-08-10 18:31:43 -0300

    add prompt template manipulation

--- plotly_agent/agent.py
+++ plotly_agent/agent.py
@@ -3,14 +3,14 @@ from plotly_agent.toolkit import create_plotly_chart, repair_plotly_code
 from plotly_agent.toolkit import prompt
 
 
-def create_plotly_agent(llm, max_interations=12, verbose=False):
+def create_plotly_agent(llm, max_interations=12, verbose=False, prompt=prompt):
 
     toolkit = [create_plotly_chart, repair_plotly_code]
 
     agent = create_openai_tools_agent(
-        llm,
-        toolkit,
-        prompt
+        llm=llm,
+        tools=toolkit,
+        prompt=prompt
     )
 
     plotly_agent = AgentExecutor(
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plotly-agent',
-    version='0.1.4',
+    version='0.1.5',
     license='MIT License',
     packages=find_packages(),
     install_requires=[
